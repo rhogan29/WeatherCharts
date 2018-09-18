d26eeb369a6897b07686bde843013f2fa4b26bdc
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-09-18 16:08:43 -0400

    Robert Hogan Initial search bar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+// bring in search bar container
+import SearchBar from '../containers/search_bar';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <SearchBar />
     );
   }
 }
---
+++ src/containers/search_bar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+// searchbar component will also contain the submit button 
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <form className="input-group">
+                <input />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
